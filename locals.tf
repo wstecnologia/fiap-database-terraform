4d7d4460bbb26d782c1451bd2785a5908cf2ca58
saviodba <saviodba@gmail.com>
2024-10-11 11:35:05 -0300

    Ajuste criação security group

--- locals.tf
+++ locals.tf
@@ -1,4 +1,3 @@
 locals {
-  subnet_ids = data.aws_subnets.available.ids
+  security_group_id = length(data.aws_security_group.existing_sg.id) > 0 ? data.aws_security_group.existing_sg.id : aws_security_group.security-group[0].id
 }
-
